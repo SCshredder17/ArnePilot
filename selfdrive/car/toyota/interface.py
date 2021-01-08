18b29319aaea8eb3c3d576b5723841ca7401650d
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2021-01-07 17:01:16 -0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -262,8 +262,8 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2]:
       stop_and_go = True
-      ret.safetyParam = 53
-      ret.wheelbase = 2.63906
+      ret.safetyParam = 50
+      ret.wheelbase = 2.7 #2.63906
       ret.steerRatio = 15.33
       tire_stiffness_factor = 0.996  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
