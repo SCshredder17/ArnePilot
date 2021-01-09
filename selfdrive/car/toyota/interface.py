971c1c1ce75ee65ba0e2612304bd6aa23b4d465f
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2021-01-08 16:01:32 -0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -263,11 +263,11 @@ class CarInterface(CarInterfaceBase):
     elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2]:
       stop_and_go = True
       ret.safetyParam = 50
-      ret.wheelbase = 2.7 #2.63906
+      ret.wheelbase = 2.67 #2.63906
       ret.steerRatio = 15.33
       tire_stiffness_factor = 0.996  # not optimized yet
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.steerActuatorDelay = 0.6
+      ret.steerActuatorDelay = 0.4
       ret.steerLimitTimer = 5.0
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGainBP = [18, 22, 26]
