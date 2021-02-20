acce20b03a25ac39bf0e4d46da5debd52ab18b62
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2021-02-20 12:44:59 -0800

    Update __init__.py

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -332,9 +332,9 @@ class DynamicFollow:
     if self.last_modified != self.modified:
       self.dp_dynamic_follow, self.dp_dynamic_follow_last_modified = param_get_if_updated("dp_dynamic_follow", "int", self.dp_dynamic_follow, self.dp_dynamic_follow_last_modified)
       self.global_df_mod, self.dp_dynamic_follow_multiplier_last_modified = param_get_if_updated("dp_dynamic_follow_multiplier", "float", self.global_df_mod, self.dp_dynamic_follow_multiplier_last_modified)
-      if self.global_df_mod != .9:
-        self.global_df_mod = clip(self.global_df_mod, .85, 9.99)
+      if self.global_df_mod != .85:
+        self.global_df_mod = clip(self.global_df_mod, .75, 9.99)
       self.min_TR, self.dp_dynamic_follow_min_tr_last_modified = param_get_if_updated("dp_dynamic_follow_min_tr", "float", self.min_TR, self.dp_dynamic_follow_min_tr_last_modified)
-      if self.min_TR != .9:
+      if self.min_TR != .85:
         self.min_TR = clip(self.min_TR, .75, 9.99)
       self.last_modified = self.modified
